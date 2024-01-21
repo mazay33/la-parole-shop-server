ed3b81dbea0c4cf360a947465ba70985e00cb0e6
a.mazaev <a.mazaev@ac.gov.ru>
2024-01-21 16:34:59 +0300

    feat: auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const ApiError = require("../exceptions/apiError");
+const tokenService = require("../service/tokenService");
+
+module.exports = function (req, res, next) {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    const accessToken = authorizationHeader.split(" ")[1];
+    if (!accessToken) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    const userData = tokenService.validateAccessToken(accessToken);
+    if (!userData) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    req.user = userData;
+    next();
+  } catch (error) {
+    return next(ApiError.UnauthorizedError());
+  }
+};
